f0a407bfe7a2e298d9f7bf3e20c16bc4414c419f
majorbruteforce <saswat.pb03@gmail.com>
2025-01-28 12:28:18 +0530

    Add locks to execute writes independently

--- src/_simulator/handleConnection.js
+++ src/_simulator/handleConnection.js
@@ -2,6 +2,7 @@ import net from 'net';
 import { defaultValues } from '../constants/default-values.js';
 
 class ConnectionHandler {
+
     constructor(
         tcpHost,
         tcpPort,
@@ -13,25 +14,65 @@ class ConnectionHandler {
         this.responseHandler = responseHandler;
         this.challengeName = challengeName;
         this.client = new net.Socket();
+        this.isConnected = false;
+        this.lock = false;
+        this.queue = [];
     }
 
     dialHost() {
-        this.client.connect(this.tcpPort, this.tcpHost, () => {
-            console.log(`Connected to ${this.challengeName} server at ${this.tcpHost}:${this.tcpPort}`);
-        }).on('data', (data) => {
-            this.responseHandler(data);
-        }).on('close', () => {
-            console.log('Connection closed');
-        }).on('error', (error) => {
-            console.error(`Error: ${error.message}`);
-        });
+        this.client
+            .connect(this.tcpPort, this.tcpHost, () => {
+                this.isConnected = true; 
+                console.log(`Connected to ${this.challengeName} server at ${this.tcpHost}:${this.tcpPort}`);
+            })
+            .on('data', (data) => {
+                this.responseHandler(JSON.stringify(data));
+                this.lock = false;
+                this.#processQueue();
+            })
+            .on('close', () => {
+                this.isConnected = false;
+                console.log('Connection closed');
+            })
+            .on('error', (error) => {
+                this.isConnected = false; 
+                console.error(`Error: ${error.message}`);
+            });
+    }
+
+    sendData(buffer) {
+        this.queue.push(buffer);
+        this.#processQueue();
+    }
+
+    #processQueue() {
+        if (this.lock || this.queue.length == 0) {
+            return;
+        }
+
+        this.lock = true;
+        const buffer = this.queue.shift();
+        this.client.write(buffer);
+    }
+
+    getStatus() {
+        if (this.client.connecting) {
+            return 'connecting';
+        } else if (this.isConnected) {
+            return 'connected';
+        } else if (this.client.destroyed) {
+            return 'disconnected';
+        } else {
+            return 'idle'; 
+        }
     }
 
     disconnect() {
-        this.client.end().catch((error) => {
-            console.error(`Error: ${error}`);
-        });
-        console.log('Disconnected from server');
+        if (this.isConnected) {
+            this.client.end();
+        } else {
+            console.error('Cannot disconnect. The client is not connected.');
+        }
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+import { set } from "mongoose";
 import ConnectionHandler from "./_simulator/handleConnection.js";
 import init from "./init.js";
 
-async function  main() {
+async function main() {
     // await init(); 
-    const connection = new ConnectionHandler('localhost', 3030);
+    const connection = new ConnectionHandler('localhost', 6379);
+    console.log(connection.getStatus());
     connection.dialHost();
-
+    console.log(connection.getStatus());
+    connection.sendData("*1\r\n$4\r\nPING\r\n");
+    connection.sendData("*1\r\n$4\r\nPING\r\n");
+    connection.sendData("*1\r\n$4\r\nPING\r\n");
+    connection.sendData("*1\r\n$4\r\nPING\r\n");
+    setTimeout(() => { 
+        console.log(connection.getStatus());
+        connection.sendData("*1\r\n$4\r\nPING\r\n");
+     }, 1000)
+    
 }
 
 main();
+
